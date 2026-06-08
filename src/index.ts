429c6a045ddf97694ae220d3ce3993f53ce369b6
Kristoffer Thøring <kristoffer.thoring@lanekassen.no>
2026-06-08 14:57:59 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export type { ButtonProps } from "./button/button";
+export { Button } from "./button/button";
+
+export type { TooltipProps } from "./tooltip/tooltip";
+export { Tooltip } from "./tooltip/tooltip";
